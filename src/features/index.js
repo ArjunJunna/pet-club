b311982b154f8c079ab672f0aec7f6a0875168f0
Arjun Junna <arjun9852@gmail.com>
2023-01-13 22:31:30 +0530

    feat : add post slice and post reducer

--- src/features/index.js
+++ src/features/index.js
@@ -1,2 +1,3 @@
 export * from './auth/authSlice';
 export * from './posts/postsSlice';
+export * from './post/postSlice';
--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer, postsReducer } from './features';
+import { authReducer, postsReducer,postReducer } from './features';
 
 export const store=configureStore({
     reducer:{
         auth:authReducer,
         posts:postsReducer,
+        post:postReducer,
     },
 });
