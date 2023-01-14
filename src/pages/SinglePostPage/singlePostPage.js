d3093fe3f237c3e63d329090435649f7b3205c5f
Arjun Junna <arjun9852@gmail.com>
2023-01-14 23:45:41 +0530

    feat : add single post

--- src/pages/SinglePostPage/singlePostPage.js
+++ src/pages/SinglePostPage/singlePostPage.js
@@ -1,18 +1,42 @@
-import React from 'react'
-import { SideBar, SuggestedUsers,Post } from '../../components';
+import { SideBar, SuggestedUsers, Post } from '../../components';
+import { getUserPost } from '../../features';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 export const SinglePostPage = () => {
+  const { data: post } = useSelector(state => state.post);
+  console.log('from single post: ', post);
+  const { data: posts } = useSelector(state => state.posts);
+  console.log('from all post: ', posts);
+
+  const dispatch = useDispatch();
+
+  const { postId } = useParams();
+  console.log('using useParam: ', postId);
+
+  useEffect(() => {
+    dispatch(getUserPost(postId));
+  }, [dispatch, postId, posts]);
+
   return (
     <>
       <div className="flex justify-evenly text-white dark:bg-slate-900 h-screen">
         <SideBar />
         <div className="flex flex-col grow max-w-xl z-10">
-          <h2 className="text-base p-2 font-medium">Your Posts</h2>
-          
+          <h2 className="text-base p-2 font-medium">Your Post</h2>
+          {post === null ? (
+            <p className="text-black text-xl m-auto dark:text-white">
+              No post to show here yet...
+            </p>
+          ) : (
+            <>
+              <Post post={post} />
+            </>
+          )}
         </div>
         <SuggestedUsers />
       </div>
     </>
   );
-}
-
+};
