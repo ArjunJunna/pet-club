bc7f5292eb2b389f935c8fd35abf183aabfad96d
Arjun Junna <arjun9852@gmail.com>
2023-01-19 16:50:27 +0530

    feat : add comment services

---
+++ src/services/commentServices.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const getAllPostCommentsService=async(postId)=>{
+    const response = await axios.get(`/api/comments/${postId}`);
+    return response;
+}
+
+const addPostCommentService=async(postId,commentData,token)=>{
+    const response = await axios.post(`/api/comments/add/${postId}`,commentData,{
+        headers:{
+            authorization:token,
+        }
+    });
+    return response;
+}
+
+const editPostCommentService=async(postId,commentId,token,commentData)=>{
+    const response = await axios.post(`/api/comments/edit/${postId}/${commentId}`,{commentData},{
+        headers:{
+            authorization:token,
+        }
+    });
+    return response;
+}
+
+const deletePostCommentService = async (postId, commentId, token) => {
+  const response = await axios.post(
+    `/api/comments/delete/${postId}/${commentId}`,
+    {},
+    { headers: { authorization: token } }
+  );
+  return response;
+};
+
+
+export {getAllPostCommentsService,addPostCommentService,editPostCommentService,deletePostCommentService};
--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,3 @@
 export * from './postServices';
 export * from './userServices';
+export * from './commentServices';
